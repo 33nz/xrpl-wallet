d326e52a38f18e80587db299af7333d92861a5d6
Jacob Wingate <jacob.wingate33@gmail.com>
2025-01-21 20:02:48 +0100

    add root route and import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { BrowserRouter, Routes, Router } from 'react-router-dom'
+
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 import Main from './pages/Main.js'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+        </Routes>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
