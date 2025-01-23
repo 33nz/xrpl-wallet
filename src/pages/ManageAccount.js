010a0f2ad02378c50d3b11bcf3c9a607be71a708
Jacob Wingate <jacob.wingate33@gmail.com>
2025-01-23 20:12:23 +0100

    add start of manage accunts page

--- src/pages/ManageAccount.js
+++ src/pages/ManageAccount.js
@@ -1,5 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faTrash, faList } from '@fortawesome/free-solid-svg-icons'
+
 import './manage-account.scss'
 
-function ManageAccount() {}
+function ManageAccount() {
+  return (
+    <div className="manage-account">
+      <h1>
+        <FontAwesomeIcon icon={faList} />
+        <span>My Accounts</span>
+      </h1>
+
+      <ul>
+        <li>
+          <div className="address">Fake rAddress</div>
+          <div className="buttons-container"></div>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export default ManageAccount
