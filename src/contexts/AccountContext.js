d4373b8dbc8fa574d2f92d9fbf9a6a34aba20f3c
Jacob Wingate <jacob.wingate33@gmail.com>
2025-02-10 19:47:24 +0100

    add account, context, component and hook for use

--- src/contexts/AccountContext.js
+++ src/contexts/AccountContext.js
@@ -1,7 +1,22 @@
-import { createContext } from 'react'
+import { createContext, useState, useContext } from 'react'
 
 // Create a context
+const AccountContext = createContext()
 
 // Provider component
+export const AccountProvider = ({ children }) => {
+  const [accounts, setAccounts] = useState([])
 
-// Custom Hook
+  const addAccount = (account) => {
+    setAccounts((prevAccounts) => [...prevAccounts, account])
+  }
+
+  return (
+    <AccountContext.Provider value={{ accounts, addAccount }}>
+      {children}
+    </AccountContext.Provider>
+  )
+}
+
+// Custom Hookß
+export const useAccounts = () => useContext(AccountContext)
